2fb454257738da708610d359f57c3a20a65c9f71
statendub <odubstaten@gmail.com>
2018-12-11 09:48:10 -0500

    collect user input examples

---
+++ lab_input/input_ip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#have user input ip addy
+user_input = input('Please type in an IPV4 address:')
+
+#show user IP 
+print("you told me the ipv4 address is:" + user_input)
+
+#collect vendor from user
+user_input= input('what vendor is this:')
+print("the vendor is:" + user_input)
+
+#when completed will collect user ip and vendor 
