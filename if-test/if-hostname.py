fa0d96665f495430dbcc69a0b55a3b2e4a7551d7
statendub <odubstaten@gmail.com>
2018-12-12 15:20:21 -0500

    matching input verification

---
+++ if-test/if-hostname.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#hostname = 'MTG':
+#if hostname == 'MTG':
+#	print('the hostname was found to be mtg')
+
+#above is template for manipulating search functions
+
+#code below will be customizations of the if function 
+
+hostname =input('what is your name?') # allows for the hostname to be declared by input
+
+if hostname.upper() =='MTG':    # accepts input from user
+	print('the hostname was found to be mtg')
+	print('hostname matches expected config')
+
+print('exiting the script')	
